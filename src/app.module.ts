be18ae2814077d646820230b28205855a5676a61
xyashino <szczypiorpan123@gmail.com>
2023-04-18 21:32:26 +0200

    Add to import: ConfigModule and TypeOrmModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeormConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
