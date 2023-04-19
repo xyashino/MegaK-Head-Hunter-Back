c8f5a68ab64f60913446944d3d145754e2505667
xyashino <szczypiorpan123@gmail.com>
2023-04-19 22:05:48 +0200

    Configure CORS and read origin from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
-  const { PORT } = process.env;
+  const { PORT , CORS} = process.env;
   const app = await NestFactory.create(AppModule);
+
+  const options: CorsOptions = {
+    origin: CORS,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  };
+  app.enableCors(options);
   await app.listen(PORT);
 }
 bootstrap();
