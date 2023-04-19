9acefb4883fda7c12938680ba6210900fc03a80c
Daniel Sulkowski <dansulk@gmail.com>
2023-04-19 17:24:03 +0200

    Added librares 'cookie-parser' and use in app for login

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: false,
@@ -16,6 +19,7 @@ async function bootstrap() {
       },
     }),
   );
+
   await app.listen(PORT);
 }
 bootstrap();
