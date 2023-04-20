e2e507e83984411963d74fe61b93de92d512928e
xyashino <szczypiorpan123@gmail.com>
2023-04-20 11:44:36 +0200

    Create SerializationInterceptor for DTO-based data serialization

---
+++ src/interceptors/serialization.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+    CallHandler,
+    ExecutionContext,
+    Injectable,
+    NestInterceptor,
+    UseInterceptors,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
+
+export function Serialize(dto: any) {
+    return UseInterceptors(new SerializationInterceptor(dto));
+}
+
+@Injectable()
+export class SerializationInterceptor implements NestInterceptor {
+    constructor(private readonly dto: any) {}
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            map((data: any) => {
+                return plainToInstance(this.dto, data, {
+                    excludeExtraneousValues: true,
+                });
+            }),
+        );
+    }
+}
