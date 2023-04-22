20e730992d53e3f0b4e4563dbe2b6f1796f167a5
xyashino <szczypiorpan123@gmail.com>
2023-04-22 22:54:47 +0200

    Implement new logic in HrService and HrController

--- src/hr/hr.controller.ts
+++ src/hr/hr.controller.ts
@@ -4,40 +4,34 @@ import {
   Post,
   Body,
   Patch,
-  Param, Inject, forwardRef, Delete, Query,
+  Param,
+  Inject,
+  forwardRef,
+  Delete,
 } from '@nestjs/common';
 import { HrService } from './hr.service';
 import { CreateHrDto } from './dto/create-hr.dto';
 import { UpdateHrDto } from './dto/update-hr.dto';
 import { RegisterHrDto } from './dto/register-hr.dto';
-import {Serialize} from "../interceptors/serialization.interceptor";
-import {ResponseHrDto} from "./dto/response-hr.dto";
-import {QueryHrDto} from "./dto/query-hr.dto";
-
+import { Serialize } from '../interceptors/serialization.interceptor';
+import { ResponseHrDto } from './dto/response-hr.dto';
 
 @Serialize(ResponseHrDto)
 @Controller('hr')
 export class HrController {
-
-  @Inject(forwardRef(()=>HrService))
-  private readonly hrService: HrService
+  @Inject(forwardRef(() => HrService))
+  private readonly hrService: HrService;
   @Post()
   create(@Body() createHrDto: CreateHrDto) {
     return this.hrService.create(createHrDto);
   }
   @Get()
-  findAll(
-      @Query() hrQuery:QueryHrDto
-  ) {
-    return this.hrService.findAll(hrQuery);
+  findAll() {
+    return this.hrService.findAll();
   }
   @Post('/register/:id')
   registerHr(@Param('id') id: string, @Body() createHrDto: RegisterHrDto) {
-    return this.hrService.register(createHrDto,id);
-  }
-  @Get('/user/:id')
-  getHrByUserId(@Param('id') id: string) {
-    return this.hrService.findOneByUserId(id);
+    return this.hrService.register(createHrDto, id);
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
