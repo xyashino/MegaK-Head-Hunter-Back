4cb336df15a4130dcfb240a180f713589d848676
Daniel Sulkowski <dansulk@gmail.com>
2023-04-24 17:15:55 +0200

    Removed comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfigAsync } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-<<<<<<<<< Temporary merge branch 1
 import { HrModule } from './hr/hr.module';
-
-=========
 import { MailModule } from './mail/mail.module';
 import { StudentsModule } from './students/students.module';
 import { UploadModule } from './upload/upload.module';
