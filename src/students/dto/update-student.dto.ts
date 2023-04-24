d27d30f37cb95ddd15bb6b93016f7b1b47728ece
Pawel Urasinski <uraspawel@gmail.com>
2023-04-24 19:30:46 +0200

    Removed fields that cannot be edited

--- src/students/dto/update-student.dto.ts
+++ src/students/dto/update-student.dto.ts
@@ -2,16 +2,13 @@ import {
   Contains,
   IsArray,
   IsBoolean,
-  IsDefined,
   IsEnum,
   IsInt,
   IsNotEmpty,
   IsOptional,
   IsString,
-  Max,
   Min,
 } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateStudentDto } from './create-student.dto';
 import { StudentTypeWork } from 'src/enums/students-type-work.enums';
 import { StudentStatus } from 'src/enums/student-status.enums';
@@ -23,35 +20,6 @@ export class UpdateStudentDto implements Partial<CreateStudentDto> {
   @IsNotEmpty()
   @Contains('@')
   email?: string;
-  
-
-  @IsInt()
-  @IsNotEmpty()
-  @Min(0)
-  @Max(5)
-  courseCompletion: number;
-
-  @IsInt()
-  @IsNotEmpty()
-  @Min(0)
-  @Max(5)
-  courseEngagment: number;
-
-  @IsInt()
-  @IsNotEmpty()
-  @Min(0)
-  @Max(5)
-  projectDegree?: number;
-
-  @IsInt()
-  @IsNotEmpty()
-  @Min(0)
-  @Max(5)
-  teamProjectDegree: number;
-
-  @IsNotEmpty()
-  @IsArray()
-  bonusProjectUrls: string[];
 
   @IsOptional()
   @IsString()
