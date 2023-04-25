ffe6afa218f8db92212b0646fb5d398b28ae0608
Daniel Sulkowski <dansulk@gmail.com>
2023-04-25 19:49:21 +0200

    restoring the original version

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const { PORT } = process.env;
+  const { PORT, CORS } = process.env;
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  // const options: CorsOptions = {
-  //   origin: CORS,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   preflightContinue: false,
-  //   credentials: true,
-  // };
-  // app.enableCors(options);
-  app.enableCors({
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  const options: CorsOptions = {
+    origin: CORS,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
     credentials: true,
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5173'],
-  });
+  };
+  app.enableCors(options);
   await app.listen(PORT);
 }
 bootstrap();
