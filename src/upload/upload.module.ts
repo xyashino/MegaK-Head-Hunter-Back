fa6ea86802f0e7130b8952cb938aecbfa84ad5fb
Daniel Sulkowski <dansulk@gmail.com>
2023-04-26 15:05:33 +0200

    changed the logic of adding students via the students service

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
-import { UsersModule } from '../users/users.module';
 import { UploadController } from './upload.controller';
 import { MailModule } from '../mail/mail.module';
+import { StudentsModule } from '../students/students.module';
 
 @Module({
-  imports: [UsersModule, MailModule],
+  imports: [MailModule, StudentsModule],
   controllers: [UploadController],
   providers: [UploadService],
 })
