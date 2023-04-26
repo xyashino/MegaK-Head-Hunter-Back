13e5d2ba783903a729dbbfc7860148573da49258
xyashino <szczypiorpan123@gmail.com>
2023-04-26 12:33:06 +0200

    Remove redundant variables, add validation to `findOne` method, change logic in `remove` method, and modify `register` method

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,9 +1,17 @@
-import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import {
+  ConflictException,
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { Student } from './entities/student.entity';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { applyDataToEntity } from '../utils/apply-data-to-entity';
+import { UserRole } from '../enums/user-role.enums';
+import { RegisterStudentDto } from './dto/register-student.dto';
 
 @Injectable()
 export class StudentsService {
@@ -12,9 +20,11 @@ export class StudentsService {
   async create({ email, ...rest }: CreateStudentDto) {
     const newStudent = new Student();
     applyDataToEntity(newStudent, rest);
-
-    const addedStudent = await newStudent.save();
-    return addedStudent;
+    newStudent.user = await this.usersService.create({
+      email,
+      role: UserRole.STUDENT,
+    });
+    return newStudent.save();
   }
 
   findAll() {
@@ -26,20 +36,29 @@ export class StudentsService {
       where: { id },
       relations: { user: true },
     });
+    if (!student) throw new NotFoundException('Invalid student Id');
     return student;
   }
 
   async remove(id: string) {
     const student = await this.findOne(id);
-    return student.remove();
+    const result = student.remove();
+    await this.usersService.remove(student.user.id);
+    return result;
   }
 
-  async update(id: string, { ...rest }: UpdateStudentDto) {
+  async update(id: string, { email, ...rest }: UpdateStudentDto) {
     const student = await this.findOne(id);
-
     applyDataToEntity(student, rest);
-    const updadedStudent = await student.save();
-
-    return updadedStudent;
+    return student.save();
+  }
+  async register(id: string, { pwd, ...rest }: RegisterStudentDto) {
+    const student = await this.findOne(id);
+    if (student.user.isActive)
+      throw new ConflictException('The user has been registered');
+    await this.usersService.update(student.user.id, { pwd });
+    applyDataToEntity(student, rest);
+    await student.save();
+    return this.findOne(id);
   }
 }
