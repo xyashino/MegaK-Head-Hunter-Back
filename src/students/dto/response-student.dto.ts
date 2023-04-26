1e3ea50b9be00cda363e0e5720872a6f98813f86
xyashino <szczypiorpan123@gmail.com>
2023-04-26 12:22:50 +0200

    Create response student DTO for data serialization

---
+++ src/students/dto/response-student.dto.ts
@@ -0,0 +1,20 @@
+import { Exclude, Expose} from 'class-transformer';
+import { User } from '../../users/entities/user.entity';
+import {Student} from "../entities/student.entity";
+
+export class ResponseStudentDto implements Partial<Student> {
+  @Exclude()
+  user: User;
+  @Expose()
+  get email() {
+    return this.user.email;
+  }
+  @Expose()
+  get isActive() {
+    return this.user.isActive;
+  }
+  @Expose()
+  get userId() {
+    return this.user.id;
+  }
+}
