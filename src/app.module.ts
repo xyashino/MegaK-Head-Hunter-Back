536e98d39248d4a318a0eb6b5786f42fef85865d
xyashino <szczypiorpan123@gmail.com>
2023-04-26 12:15:10 +0200

    Remove unused Nest files controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -25,7 +23,7 @@ import { StudentsModule } from './students/students.module';
     ConsoleModule,
     StudentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, AdminCommand],
+  controllers: [],
+  providers: [AdminCommand],
 })
 export class AppModule {}
