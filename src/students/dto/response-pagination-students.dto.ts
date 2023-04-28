c1207d9e6ccb6f56ccbcfaf77e09c8c45951fc89
Daniel Sulkowski <dansulk@gmail.com>
2023-04-28 22:44:06 +0200

    renamed variable name

--- src/students/dto/response-pagination-students.dto.ts
+++ src/students/dto/response-pagination-students.dto.ts
@@ -2,11 +2,13 @@ import { ResponseStudentDto } from './response-student.dto';
 import { PageMetaDto } from '../../common/dtos/page/page-meta.dto';
 import { Expose, Transform } from 'class-transformer';
 import { Student } from '../entities/student.entity';
-import { arrayDataToDto } from '../../utils/mappers/array-data-to-dto.mapper';
+import { transformArrayDataToDtoInstance } from '../../utils/mappers/array-data-to-dto.mapper';
 
 export class ResponsePaginationStudentsDto {
   @Expose()
-  @Transform(({ obj }) => arrayDataToDto(obj.data, ResponseStudentDto))
+  @Transform(({ obj }) =>
+    transformArrayDataToDtoInstance(obj.data, ResponseStudentDto),
+  )
   data: Student[];
 
   @Expose()
