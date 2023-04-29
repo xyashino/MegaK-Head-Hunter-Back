1893e8a9990e5435a6dd6735a3cddcd438384e4c
Daniel Sulkowski <dansulk@gmail.com>
2023-04-29 17:37:49 +0200

    Added booking_date to Interview Entity

--- src/interview/entities/interview.entity.ts
+++ src/interview/entities/interview.entity.ts
@@ -1,5 +1,6 @@
 import {
   BaseEntity,
+  BeforeInsert,
   Column,
   Entity,
   JoinColumn,
@@ -14,8 +15,17 @@ export class Interview extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({
+    type: 'timestamp',
+    default: null,
+  })
   created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: null,
+  })
+  booking_date: Date;
 
   @ManyToOne(() => Hr, (hr) => hr.interviews)
   @JoinColumn()
@@ -24,4 +34,11 @@ export class Interview extends BaseEntity {
   @ManyToOne(() => Student, (student) => student.interviews)
   @JoinColumn()
   student: Student;
+
+  @BeforeInsert()
+  setDate() {
+    const tenDaysInMs = 10 * 24 * 60 * 60 * 1000;
+    this.created_at = new Date();
+    this.booking_date = new Date(this.created_at.getTime() + tenDaysInMs);
+  }
 }
