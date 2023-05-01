342995009db1116914727531e2801e391633506d
xyashino <szczypiorpan123@gmail.com>
2023-05-01 18:23:25 +0200

    Modify ResponseUserDto for new serialization logic

--- src/users/dto/response-user.dto.ts
+++ src/users/dto/response-user.dto.ts
@@ -1,12 +1,28 @@
-import { Exclude } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { User } from '../entities/user.entity';
 import { UserRole } from '../../enums/user-role.enums';
+import { Hr } from '../../hr/entities/hr.entity';
+import { Student } from '../../students/entities/student.entity';
 
 export class ResponseUserDto implements Partial<User> {
-  @Exclude()
+  @Expose()
   email: string;
-  @Exclude()
+  @Expose()
   isActive: boolean;
-  @Exclude()
+  @Expose()
   role: UserRole;
+
+  @Expose()
+  @Transform(({ obj }) => {
+    const { hr, student }: { hr: Hr | null; student: Student | null } = obj;
+    if (hr) return { id: hr.id, fullName: hr.fullName };
+    if (student)
+      return {
+        id: student.id,
+        fullName: `${student.firstname} ${student.lastname}`,
+        githubUsername: student.githubUsername
+      };
+    return null;
+  })
+  data: any;
 }
