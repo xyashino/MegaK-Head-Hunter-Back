9ddc774d5ba6a36c311b7adb3f596642facc2038
xyashino <szczypiorpan123@gmail.com>
2023-05-01 18:22:22 +0200

    Create "CurrentUserResponse" types

---
+++ src/types/users/current-user-response.interface.ts
@@ -0,0 +1,29 @@
+import {UserResponseBase} from "./user-response";
+
+interface UserCurrentAdminResponse extends UserResponseBase {
+  isActive: true;
+  role: 'admin';
+  data: null;
+}
+interface UserCurrentHrResponse extends UserResponseBase {
+  isActive: boolean;
+  data: {
+    id: string;
+    fullName: string;
+  };
+}
+
+interface UserCurrentStudentResponse extends UserResponseBase {
+  role: 'hr';
+  isActive: boolean;
+  data: {
+    id: string;
+    fullName: string;
+    githubUsername: string;
+  };
+}
+
+export type CurrentUserResponse =
+  | UserCurrentHrResponse
+  | UserCurrentAdminResponse
+  | UserCurrentStudentResponse;
