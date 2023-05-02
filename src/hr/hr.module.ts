285a91ed84eafc24bcce75cf182e18b2df347607
Daniel Sulkowski <dansulk@gmail.com>
2023-05-02 21:19:13 +0200

    added login after register hr

--- src/hr/hr.module.ts
+++ src/hr/hr.module.ts
@@ -4,9 +4,15 @@ import { HrController } from './hr.controller';
 import { UsersModule } from '../users/users.module';
 import { MailModule } from '../mail/mail.module';
 import { InterviewModule } from '../interview/interview.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MailModule, forwardRef(() => InterviewModule)],
+  imports: [
+    UsersModule,
+    MailModule,
+    forwardRef(() => InterviewModule),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [HrController],
   providers: [HrService],
   exports: [HrService],
