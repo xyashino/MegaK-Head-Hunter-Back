8d9c083feef7dbf67924e7a100a049c26a4e9977
Pawel Urasinski <uraspawel@gmail.com>
2023-05-03 21:05:12 +0200

    Added student filtering options

--- src/filtration/filtration.service.ts
+++ src/filtration/filtration.service.ts
@@ -14,6 +14,50 @@ export class FiltrationService {
 
       // sql queries
 
-      
+      .andWhere('student.expectedTypeWork = :expectedTypeWork ', {
+        expectedTypeWork: String(mark.expectedTypeWork || ''),
+      })
+
+      .andWhere('student.expectedContractType = :expectedContractType', {
+        expectedContractType: '' || String(mark.expectedContractType || ''),
+      })
+
+      .andWhere('student.courseEngagement >= :courseEngagement', {
+        courseEngagement:
+          mark.courseEngagement === undefined
+            ? 1
+            : Number(mark.courseEngagement),
+      })
+      .andWhere('student.courseCompletion >= :courseCompletion', {
+        courseCompletion:
+          mark.projecourseCompletionctDegree === undefined
+            ? 1
+            : Number(mark.courseCompletion),
+      })
+      .andWhere('student.projectDegree >= :projectDegree', {
+        projectDegree:
+          mark.projectDegree === undefined ? 1 : Number(mark.projectDegree),
+      })
+      .andWhere('student.teamProjectDegree >= :teamProjectDegree', {
+        teamProjectDegree:
+          mark.teamProjectDegree === undefined
+            ? 1
+            : Number(mark.teamProjectDegree),
+      })
+      .andWhere('student.canTakeApprenticeship = :canTakeApprenticeship', {
+        canTakeApprenticeship: Boolean(mark.canTakeApprenticeship),
+      })
+      .andWhere('student.monthsOfCommercialExp >= :monthsOfCommercialExp', {
+        monthsOfCommercialExp:
+          mark.monthsOfCommercialExp === undefined
+            ? 0
+            : Number(mark.monthsOfCommercialExp),
+      })
+      .andWhere('student.expectedSalary BETWEEN :minSalary AND :maxSalary', {
+        minSalary: mark.minSalary === undefined ? 0 : Number(mark.minSalary),
+        maxSalary: mark.maxSalary === undefined ? 0 : Number(mark.maxSalary),
+      })
+
+      .getMany();
   }
 }
