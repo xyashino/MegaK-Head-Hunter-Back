3393206ffbe6a29db3d48772b46325673e9d7970
xyashino <szczypiorpan123@gmail.com>
2023-05-09 19:59:43 +0200

    Update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
 export * from './users';
 export * from './students';
 export * from './hr';
-export * from './EnumTypes'
+export * from './EnumTypes';
+export * from './pagination';
+export * from './interview';
--- src/types/interview/interview-find-response.ts
+++
@@ -1,7 +0,0 @@
-import { InterviewResponse } from './interview-response';
-import { PageMeta } from '../pagination/page-meta';
-
-export interface InterviewFindResponse {
-  data: InterviewResponse[];
-  meta: PageMeta;
-}
