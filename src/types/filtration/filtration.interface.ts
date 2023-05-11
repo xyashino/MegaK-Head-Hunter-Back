97a78dc29762bf0837bf1a57a88ad8523810b846
xyashino <szczypiorpan123@gmail.com>
2023-05-11 13:31:42 +0200

    Update types

--- src/types/filtration/filtration.interface.ts
+++ src/types/filtration/filtration.interface.ts
@@ -6,8 +6,8 @@ export interface Filtration {
   courseEngagement: number | string;
   projectDegree: number | string;
   teamProjectDegree: number | string;
-  expectedTypeWork: ExpectedTypeWorkEnum;
-  expectedContractType: ExpectedContractTypeEnum;
+  expectedTypeWork: ExpectedTypeWorkEnum | '';
+  expectedContractType: ExpectedContractTypeEnum | '';
   minSalary: number | string;
   maxSalary: number | string;
   canTakeApprenticeship: number | string;
