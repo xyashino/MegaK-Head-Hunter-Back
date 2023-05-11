64ca717b44fc3ba750b8a61e7768cdf4ddf80ca7
Daniel Sulkowski <dansulk@gmail.com>
2023-05-11 18:14:39 +0200

    added monthsOfCommercialExp to response student DTO

--- src/students/dto/response-student.dto.ts
+++ src/students/dto/response-student.dto.ts
@@ -2,6 +2,7 @@ import { Expose, Transform } from 'class-transformer';
 import { Student } from '../entities/student.entity';
 import { StudentStatus } from '../../enums/student-status.enums';
 import { StudentContactType } from '../../enums/student-contract-type.enums';
+import { Column } from 'typeorm';
 
 export class ResponseStudentDto implements Partial<Student> {
   @Expose()
@@ -79,4 +80,7 @@ export class ResponseStudentDto implements Partial<Student> {
 
   @Expose()
   canTakeApprenticeship: boolean;
+
+  @Expose()
+  monthsOfCommercialExp: number;
 }
