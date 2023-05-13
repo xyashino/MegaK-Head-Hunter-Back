8e573cb422c0a6e54eff6d5493a9046d49b355a7
xyashino <szczypiorpan123@gmail.com>
2023-05-13 16:20:17 +0200

    Add guards

--- src/interview/interview.controller.ts
+++ src/interview/interview.controller.ts
@@ -18,17 +18,18 @@ import { User } from '@users/entities/user.entity';
 import { UserObj } from '@decorators/user-obj.decorator';
 
 @Controller('interview')
-@UseGuards(AuthGuard('jwt'))
 export class InterviewController {
   constructor(private readonly interviewService: InterviewService) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   @Serialize(CreateInterviewResponseDto)
   create(@Body('studentId') studentId: string, @UserObj() user: User) {
     return this.interviewService.createInterview(studentId, user);
   }
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   @Serialize(ResponseFindInterviewDto)
   findAllInterview(
     @Query() searchOptions: SearchOptionsDto,
@@ -38,12 +39,14 @@ export class InterviewController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   @Serialize(CreateInterviewResponseDto)
   findOne(@Param('id') id: string) {
     return this.interviewService.findOne(id);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   @Serialize(CreateInterviewResponseDto)
   remove(@Param('id') studentId: string, @UserObj() user: User) {
     return this.interviewService.removeInterview(studentId, user);
